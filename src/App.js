b9fb4b0e274f29acc70b91613d394ed7716d6c0c
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2022-10-12 20:57:37 +1100

    fix deploy bug

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  Redirect,
+} from "react-router-dom";
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
@@ -14,18 +20,19 @@ function App() {
         <Header />
         <Routes>
           {/*http://localhost:3000/ */}
-          <Route path="/">
-            <Route index element={<Navigate to="About" />} />
-            {/*http://localhost:3000/About */}
-            <Route path="About" element={<About />} />
-            {/*http://localhost:3000/Portfolio */}
-            <Route path="Portfolio" element={<Portfolio />} />
 
-            {/*http://localhost:3000/Contact */}
-            <Route path="Contact" element={<Contact />} />
-            {/*http://localhost:3000/Resume */}
-            <Route path="Resume" element={<Resume />} />
-          </Route>
+          {/* <Route path="/"> */}
+          <Route index element={<Navigate to="About" />} />
+          {/*http://localhost:3000/About */}
+          <Route path="About" element={<About />} />
+          {/*http://localhost:3000/Portfolio */}
+          <Route path="Portfolio" element={<Portfolio />} />
+
+          {/*http://localhost:3000/Contact */}
+          <Route path="Contact" element={<Contact />} />
+          {/*http://localhost:3000/Resume */}
+          <Route path="Resume" element={<Resume />} />
+          {/* </Route> */}
         </Routes>
         <Footer />
       </BrowserRouter>
